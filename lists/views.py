5a722a14be68c18c98706603f7efab36eea7c54b
Carolwing <1251503654@qq.com>
2020-05-30 04:59:20 +0800

    7_3

---
+++ lists/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect,render
+from django.http import HttpResponse
+from lists.models import Item, List
+
+# Create your views here.
+# home_page = None
+
+def home_page(request):
+    return render(request, 'home.html')
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items':items})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
